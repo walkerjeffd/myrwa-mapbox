395cf8182b61cd27098559cbd34afbd865233265
Jeff Walker <walker.jeff.d@gmail.com>
2015-11-09 15:43:06 -0500

    script to convert shp to geojson

---
+++ gis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#convert basins shapefile to geojson for uploading to mapbox editor
+
+names="watershed subbasins hydro_25k_line"
+
+mkdir -p geojson
+
+convert() {
+  echo Converting $name...
+
+  rm -f ./geojson/$name.geojson
+  ogr2ogr -f GeoJSON ./geojson/$name.geojson ~/Dropbox/Work/mystic/gis/$name.shp
+}
+
+for name in $names; do
+  convert $name
+done
+
+echo Done!
